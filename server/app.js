35100c9de9a24a3be1c26ffa95624ede1f3f7db0
Peter Cunha <petercunha8@gmail.com>
2017-09-24 11:20:31 -0400

    Eat da food

--- server/app.js
+++ server/app.js
@@ -28,11 +28,13 @@ io.on('connection', function (socket) {
       if (posX > 0) posX--
     if (data.direction == "right")
       if (posX < gameState[0].length-1) posX++
+    action()
     draw()
   })
 })
 
 var posX = 5, posY = 5
+var foodX = 2, foodY = 2
 var gameState = [
   [0, 0, 0, 0, 0, 0, 0, 0], 
   [0, 0, 0, 0, 0, 0, 0, 0], 
@@ -44,11 +46,28 @@ var gameState = [
   [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 gameState[posX][posY] = 1
+gameState[foodX][foodY] = 2
+
+// Fun stuff
+var rotation = 0.1
+
+function action() {
+  if (posX == foodX && posY == foodY) {
+    rotation += 0.1
+    io.sockets.emit('update', { rotation: rotation })
+
+    // New food
+    foodX = Math.floor(Math.random() * 8)
+    foodY = Math.floor(Math.random() * 8)
+    gameState[foodX][foodY] = 2
+  }
+}
 
 function draw() {
   for (var i = 0; i < gameState.length; i++)
     for (var j = 0; j < gameState.length; j++)
-      gameState[i][j] = 0
+      if (gameState[i][j] != 2)
+        gameState[i][j] = 0
   gameState[posX][posY] = 1
 }
 
