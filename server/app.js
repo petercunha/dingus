a01c70aeb9ebadc8b887377579cb16a6245a8b60
Peter Cunha <petercunha8@gmail.com>
2017-09-24 15:35:13 -0400

    Add special ability: Wall

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ io.on('connection', function(socket) {
 
 	// Add player and get reference as "me" variable
 	players.push(new Player())
-	var playerIndex = players.length-1
+	var playerIndex = players.length - 1
 	var me = players[playerIndex]
 
 	socket.emit('state', {
@@ -37,14 +37,18 @@ io.on('connection', function(socket) {
 			return
 		}
 
-    if (data.direction == "up")
-      if (me.y > 0) me.updatePos(0, -1)
-    if (data.direction == "down")
-      if (me.y < gameState.length-1) me.updatePos(0, 1)
-    if (data.direction == "left")
-      if (me.x > 0) me.updatePos(-1, 0)
-    if (data.direction == "right")
-      if (me.x < gameState[0].length-1) me.updatePos(1, 0)
+		// Special ability
+		if (data.direction == "space")
+			me.special.active = true
+
+		if (data.direction == "up")
+			if (me.y > 0) me.updatePos(0, -1)
+		if (data.direction == "down")
+			if (me.y < gameState[0].length - 1) me.updatePos(0, 1)
+		if (data.direction == "left")
+			if (me.x > 0) me.updatePos(-1, 0)
+		if (data.direction == "right")
+			if (me.x < gameState.length - 1) me.updatePos(1, 0)
 
 		me.action()
 		me.draw()
@@ -59,21 +63,95 @@ io.on('connection', function(socket) {
 	console.log("Online players: " + players.length);
 })
 
+var players = []
+var boardSize = 24
+var foodX = 2,
+	foodY = 2
+var gameState = zeros([boardSize, boardSize / 2])
+// Set players positions
+for (var i = 0; i < players.length; i++) {
+	gameState[players[i].x, players[i].y] = players[i]
+}
+gameState[foodX][foodY] = 2
+
+function updateClients() {
+	for (var i = 0; i < gameState.length; i++)
+		for (var j = 0; j < gameState.length; j++)
+			if (gameState[i][j]) {
+
+				// Shrink players
+				if (gameState[i][j].size > 1.25) {
+					gameState[i][j].size -= 0.001 + gameState[i][j].size * 0.003
+				}
+
+				// Tick down the walls
+				if (gameState[i][j].type == "WALL") {
+					gameState[i][j].duration--
+					if (gameState[i][j].duration < 0) {
+						gameState[i][j] = 0
+					}
+				}
+			}
+
+	io.sockets.emit('state', {
+		state: gameState
+	})
+}
+setInterval(updateClients, 50);
+
+
+// For initializing gamestate
+function zeros(dimensions) {
+	var array = [];
+	for (var i = 0; i < dimensions[0]; ++i) {
+		array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+	}
+	return array;
+}
+
+// WALL OBJECT
+function Wall() {
+	this.type = "WALL",
+	this.duration = 50
+}
+
+// PLAYER OBJECT
 function Player() {
-		this.id = Math.floor(Math.random() * 93513).toString()
-		this.alive = true,
-	  this.x = 5,
+	this.id = Math.floor(Math.random() * 93513).toString()
+	this.alive = true,
+		this.x = 5,
 		this.y = 5,
 		this.oldX = this.x,
 		this.oldY = this.y,
 		this.size = 1,
 		this.rotation = 0,
+
+		this.special = {
+			active: false,
+			name: "WALL",
+			blocks: 15
+		},
+
 		this.updatePos = function(dx, dy) {
 			this.oldX = this.x
 			this.oldY = this.y
 			this.x += dx
 			this.y += dy
+
+			if (this.special.active) {
+
+				// WALL ABILITY
+				if (this.special.name == "WALL") {
+					if (this.special.blocks < 0) {
+						this.special.active = false
+					} else {
+						gameState[this.oldX][this.oldY] = new Wall()
+						this.special.blocks--
+					}
+				}
+			}
 		},
+
 		this.action = function() {
 			// Food collision
 			if (this.x == foodX && this.y == foodY) {
@@ -81,12 +159,20 @@ function Player() {
 				this.size += 0.5
 
 				// New food
-				foodX = Math.floor(Math.random() * 8)
-				foodY = Math.floor(Math.random() * 8)
+				foodX = Math.floor(Math.random() * boardSize)
+				foodY = Math.floor(Math.random() * boardSize / 2)
 				gameState[foodX][foodY] = 2
 			}
 
 			var enemy = gameState[this.x][this.y];
+
+			// Check for walls
+			if (enemy.type == "WALL") {
+				gameState[this.oldX][this.oldY] = 0
+				this.alive = false
+			}
+
+			// Check for other players
 			if (enemy != 0 && this.x == enemy.x && this.y == enemy.y) {
 				if (enemy.size > this.size) {
 					enemy.size += this.size
@@ -115,34 +201,3 @@ function Player() {
 			}
 		}
 }
-var players = []
-
-var foodX = 2,
-	foodY = 2
-var gameState = [
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0]
-]
-// Set players positions
-for (var i = 0; i < players.length; i++) {
-	gameState[players[i].x, players[i].y] = players[i]
-}
-gameState[foodX][foodY] = 2
-
-function updateClients() {
-	for (var i = 0; i < gameState.length; i++)
-		for (var j = 0; j < gameState.length; j++)
-			if (gameState[i][j].size > 1.25)
-				gameState[i][j].size -= 0.002 + gameState[i][j].size * 0.005
-
-	io.sockets.emit('state', {
-		state: gameState
-	})
-}
-setInterval(updateClients, 50);
