3e0616eab13cf71116286a080d7d5e05fb93e0b7
Peter Cunha <petercunha8@gmail.com>
2017-09-24 10:13:23 -0400

    Basic server stuff

---
+++ server/app.js
@@ -0,0 +1,54 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500)
+      return res.end('Error loading index.html')
+    }
+
+    res.writeHead(200)
+    res.end(data)
+  })
+}
+
+io.on('connection', function (socket) {
+  socket.emit('state', { state: gameState })
+  socket.on('move', function (data) {
+    if (data.direction == "up")
+      if (posY > 0) posY--
+    if (data.direction == "down")
+      if (posY < gameState.length-1) posY++
+    if (data.direction == "left")
+      if (posX > 0) posX--
+    if (data.direction == "right")
+      if (posY < gameState.length-1) posX++
+    draw()
+    socket.emit('state', { state: gameState })
+  })
+})
+
+var posX = 0, posY = 0
+var gameState = [
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0]
+]
+gameState[posX][posY] = 1
+
+function draw() {
+  for (var i = 0; i < gameState.length; i++)
+    for (var j = 0; j < gameState.length; j++)
+      gameState[i][j] = 0
+  gameState[posX][posY] = 1
+}
